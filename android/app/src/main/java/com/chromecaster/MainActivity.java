5c5037dcaecbf77f4d5576f463dcfc0bd21c794c
nielsladekarl <nielsladekarl@gmail.com>
2020-09-19 02:35:46 +0200

    Detect playing video

--- android/app/src/main/java/com/chromecaster/MainActivity.java
+++ android/app/src/main/java/com/chromecaster/MainActivity.java
@@ -1,8 +1,9 @@
 package com.chromecaster;
 
 import com.facebook.react.ReactActivity;
+import com.facebook.react.GoogleCastActivity;
 
-public class MainActivity extends ReactActivity {
+public class MainActivity extends GoogleCastActivity {
 
   /**
    * Returns the name of the main component registered from JavaScript. This is used to schedule
---
+++ src/Home.js
@@ -0,0 +1,52 @@
+import React, {Component} from 'react';
+import {WebView} from 'react-native-webview';
+
+class Home extends Component {
+
+    _onMessage = event => {
+        console.log('_onMessage', JSON.parse(event.nativeEvent.data));
+        const res = JSON.parse(event.nativeEvent.data);
+        var videoSrc = res.message;
+    };
+
+    detectVideoPlayingJs = function () {
+        return `
+            (function () {
+                window.addEventListener("load", function() { 
+                    Object.defineProperty(HTMLMediaElement.prototype, 'playing', {
+                        get: function(){
+                            return !!(this.currentTime > 0 && !this.paused && !this.ended && this.readyState > 2);
+                        }
+                    })
+                    setInterval(function() {
+                        var videos = document.querySelectorAll('video');
+                        for(var i = 0; i < videos.length; i++) {
+                            var video = videos[i];
+                            if(video.playing) {
+                                window.ReactNativeWebView.postMessage(JSON.stringify({type: "click", message: video.src}));
+                            }
+                        }
+                    }, 1000);
+                });
+            })();
+        `;
+    };
+
+    render() {
+        return (
+            <WebView
+                source={{uri: 'https://google.com'}}
+                allowsInlineMediaPlayback={true}
+                javaScriptEnabled={true}
+                domStorageEnabled={true}
+                originWhitelist={['*']}
+                javaScriptCanOpenWindowsAutomatically={false}
+                injectedJavaScript={this.detectVideoPlayingJs()}
+                mediaPlaybackRequiresUserAction={true}
+                onMessage={this._onMessage}
+            />
+        );
+    }
+}
+
+export default Home;
